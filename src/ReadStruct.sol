c879ef2424a648eb4eef24101bfb787cf40fa2d0
Jeffrey Scholz <scholz.jeffrey@gmail.com>
2025-03-16 18:18:28 +0700

    update some tests

--- src/ReadStruct.sol
+++ src/ReadStruct.sol
@@ -17,6 +17,7 @@ contract ViewContract {
 contract ReadStruct {
     function main(address a) public view returns (uint256 x, uint256 y) {
         // read the struct from ViewContract and return the x and y values
+        // in reversed order, i.e. x is y and y is x
         // do not redeclare the struct in this contract or 
         // reference it in ViewContract
     }
