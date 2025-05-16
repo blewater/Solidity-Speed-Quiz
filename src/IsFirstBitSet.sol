b479e3fcc30aef1a2a6c704710ed1e9a78f03a57
Mario Karagiorgas <salem8@gmail.com>
2025-05-16 09:58:08 +0300

    feat: IsFirstBitSet

--- src/IsFirstBitSet.sol
+++ src/IsFirstBitSet.sol
@@ -2,9 +2,10 @@
 pragma solidity 0.8.28;
 
 contract IsFirstBitSet {
-    function main(bytes32 x) public returns (bool) {
+    function main(bytes32 x) pure public returns (bool) {
         // return true if the first bit is set
         // for example 0x000....0001 returns true
         // for example 0x000....0010 returns false
+        return uint8(x[31]) & 0x01 == 0x01;
     }
 }
