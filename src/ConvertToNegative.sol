372b172cbef0aa23b150efdb24669d62371386a5
Mario Karagiorgas <salem8@gmail.com>
2025-05-16 12:54:17 +0300

    feat: convertToNegative

--- src/ConvertToNegative.sol
+++ src/ConvertToNegative.sol
@@ -8,5 +8,15 @@ contract ConvertToNegative {
         // return -x
         // if -x cannot be computed, revert with "TooBig()"
         // do not hardcode any constants
+
+        // abs(minimum negative) is +1 than abs(int256.max)
+        require(x <= uint(type(int256).max)+1, TooBig());
+
+        // special case: abs(type(int256).min)
+        if (x == uint(type(int).max)+1) {
+            return type(int256).min;
+        }
+
+        return -int256(x);
     }
 }
