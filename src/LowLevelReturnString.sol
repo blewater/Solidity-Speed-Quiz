6cc3b6e2d291bb1ad86ed7f9522ca03785764d56
Mario Karagiorgas <salem8@gmail.com>
2025-05-18 15:11:33 +0300

    feat: low level return uint, string

--- src/LowLevelReturnString.sol
+++ src/LowLevelReturnString.sol
@@ -1,13 +1,28 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.8.28;
 
+interface IBar {
+    function baz() external returns (string memory);
+}
+
 contract LowLevelReturnString {
     function main(address a) public returns (string memory) {
         // call function "baz()" on address a
         // do not use an interface
         // baz() returns a string. Return the string.
-        // However, if baz reverts, return an empty string 
+        // However, if baz reverts, return an empty string
 
         // bonus challenge: use an interface and a high level call to accomplish the same task
+        (bool ok, bytes memory ret) = a.call(abi.encodeWithSignature("baz()"));
+        if (!ok) {
+            return "";
+        }
+        string memory ret1 = abi.decode(ret, (string));
+
+        string memory ret2 = IBar(a).baz();
+
+        assert(keccak256(bytes(ret1)) == keccak256(bytes(ret2)));
+
+        return ret1;
     }
 }
