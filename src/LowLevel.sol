dc23c40f4f6e7f14f10ef08e4081c268ed434264
Mario Karagiorgas <salem8@gmail.com>
2025-05-18 14:36:50 +0300

    feat: low level

--- src/LowLevel.sol
+++ src/LowLevel.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.8.28;
 
+    interface IFoo{
+        function foo() external;
+    }
+
+
 contract LowLevel {
     function main(address a) public returns (bool) {
         // call function "foo()" on address a
@@ -9,6 +14,12 @@ contract LowLevel {
         // return false if the call failed
 
         // bonus challenge: use an interface and a high level call to accomplish the same task
-
+        (bool success1, ) = a.call(hex"c2985578");
+        (bool success2, ) = a.call(abi.encodeWithSignature("foo()"));
+        IFoo f = IFoo(a);
+        bool success3 = false;
+        try f.foo() { success3 = true; } catch {}
+        assert(success1 == success2 && success2 == success3);
+        return success2;
     }
 }
